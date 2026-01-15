e0d3c128b6d1fc7a9248c0ceb4d42c465a155a57
Ulysses Legaspi <ulysses.legaspi@gmail.com>
2026-01-15 17:44:01 +0800

    Fix Rubocop reported offenses.

--- app/models/weather_snapshot.rb
+++ app/models/weather_snapshot.rb
@@ -1,5 +1,8 @@
+# frozen_string_literal: true
+
+# WeatherSnapshot
 class WeatherSnapshot < ApplicationRecord
   validates :city, presence: true
-  validates :temperature_c presence: true
+  validates :temperature_c, presence: true
   validates :wind_speed, presence: true
 end
