209a1f53666cce640a16fadc1e495657a8aff2fe
Ulysses Legaspi <ulysses.legaspi@gmail.com>
2026-01-16 06:23:31 +0800

    Code refactor to fix reported rubocop offenses.

--- app/services/weather/providers/open_weather_map.rb
+++ app/services/weather/providers/open_weather_map.rb
@@ -4,22 +4,39 @@ module Weather
   module Providers
     # app/services/weather/providers/open_weather_map.rb
     class OpenWeatherMap < Base
+      ENDPOINT = 'https://api.openweathermap.org/data/2.5/weather'
+      CITY = 'melbourne,AU'
+      UNITS = 'metric'
+
       def fetch
-        response = connection.get(
-          'https://api.openweathermap.org/data/2.5/weather',
-          q: 'melbourne,AU',
-          appid: ENV.fetch('OPENWEATHER_API_KEY'),
-          units: 'metric'
-        )
+        response = fetch_weather
+        return unless response&.success?
 
-        return unless response.success?
-
-        {
-          temperature_degrees: response.body.dig('main', 'temp').to_i,
-          wind_speed: response.body.dig('wind', 'speed').to_i
-        }
+        extract_weather(response.body)
       rescue Faraday::Error
         nil
+      end
+
+      private
+
+      def fetch_weather
+        connection.get(
+          ENDPOINT,
+          q: CITY,
+          appid: api_key,
+          units: UNITS
+        )
+      end
+
+      def extract_weather(body)
+        {
+          temperature_degrees: body.dig('main', 'temp')&.to_i,
+          wind_speed: body.dig('wind', 'speed')&.to_i
+        }
+      end
+
+      def api_key
+        ENV.fetch('OPENWEATHER_API_KEY')
       end
     end
   end
