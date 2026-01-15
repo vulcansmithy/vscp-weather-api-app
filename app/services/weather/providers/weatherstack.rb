f4a3c9630d6b509669f44a0e91c7a74f96ac1566
Ulysses Legaspi <ulysses.legaspi@gmail.com>
2026-01-16 06:15:31 +0800

    Code refactor. Fix rubocop reported offenses.

--- app/services/weather/providers/weatherstack.rb
+++ app/services/weather/providers/weatherstack.rb
@@ -1,24 +1,39 @@
 # frozen_string_literal: true
 
+require 'rails_helper'
+
 module Weather
   module Providers
     # app/services/weather/providers/weatherstack.rb
     class Weatherstack < Base
       def fetch
-        response = connection.get(
-          'http://api.weatherstack.com/current',
-          access_key: ENV.fetch('WEATHERSTACK_API_KEY'),
-          query: 'Melbourne'
-        )
+        response = fetch_weather
+        return unless response&.success?
 
-        return unless response.success?
-
-        {
-          temperature_degrees: response.body.dig('current', 'temperature'),
-          wind_speed: response.body.dig('current', 'wind_speed')
-        }
+        extract_weather(response.body)
       rescue Faraday::Error
         nil
+      end
+
+      private
+
+      def fetch_weather
+        connection.get(
+          'http://api.weatherstack.com/current',
+          access_key: api_key,
+          query: 'Melbourne'
+        )
+      end
+
+      def extract_weather(body)
+        {
+          temperature_degrees: body.dig('current', 'temperature'),
+          wind_speed: body.dig('current', 'wind_speed')
+        }
+      end
+
+      def api_key
+        ENV.fetch('WEATHERSTACK_API_KEY')
       end
     end
   end
