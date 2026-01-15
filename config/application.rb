ac4f8dba7323bd7bef553a5aa6c75edeec20ff58
Ulysses Legaspi <ulysses.legaspi@gmail.com>
2026-01-15 17:58:55 +0800

    Another set changes to fix Rubocop reported offenses.

--- config/application.rb
+++ config/application.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_relative 'boot'
 
 require 'rails'
@@ -19,6 +21,7 @@ require 'rails/test_unit/railtie'
 Bundler.require(*Rails.groups)
 
 module WeatherApi
+  # Application
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
--- db/migrate/20260115090354_create_weather_snapshots.rb
+++ db/migrate/20260115090354_create_weather_snapshots.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# CreateWeatherSnapshots
 class CreateWeatherSnapshots < ActiveRecord::Migration[7.1]
   def change
     create_table :weather_snapshots do |t|
