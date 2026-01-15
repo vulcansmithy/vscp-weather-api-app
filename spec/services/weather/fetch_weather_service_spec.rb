a0945785e41a56cd99e96a0e65db84ef824d1be8
Ulysses Legaspi <ulysses.legaspi@gmail.com>
2026-01-16 05:59:13 +0800

    Refactor the code to remove reported rubocop offenses.

--- spec/services/weather/fetch_weather_service_spec.rb
+++ spec/services/weather/fetch_weather_service_spec.rb
@@ -4,11 +4,24 @@ require 'rails_helper'
 
 # spec/services/weather/fetch_weather_service_spec.rb
 RSpec.describe Weather::FetchWeatherService do
-  it 'returns unified weather data' do
-    allow_any_instance_of(Weather::Providers::Weatherstack)
-      .to receive(:fetch)
-      .and_return({ temperature_degrees: 29, wind_speed: 20 })
+  let(:provider) { instance_double(Weather::Providers::Weatherstack) }
 
+  before do
+    allow(Weather::Providers::Weatherstack)
+      .to receive(:new)
+      .and_return(provider)
+
+    allow(provider)
+      .to receive(:fetch)
+      .and_return(
+        {
+          temperature_degrees: 29,
+          wind_speed: 20
+        }
+      )
+  end
+
+  it 'returns unified weather data', :aggregate_failures do
     result = described_class.new.call
 
     expect(result[:temperature_degrees]).to eq(29)
