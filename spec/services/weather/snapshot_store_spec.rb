1a36290893bc8708df648211488259fd3ff5d9ed
Ulysses Legaspi <ulysses.legaspi@gmail.com>
2026-01-28 12:56:09 +0800

    Flesh out the rspec test for SnaptshotStore service.

---
+++ spec/services/weather/snapshot_store_spec.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+# spec/services/weather/snapshot_store_spec.rb
+RSpec.describe Weather::SnapshotStore do
+  let(:redis) { Redis.new }
+  let(:store) { described_class.new(redis: redis) }
+  let(:city) { 'melbourne' }
+  let(:data) { { temperature_degrees: 22, wind_speed: 10 } }
+
+  before { redis.flushdb }
+
+  describe '#write and #read' do
+    it 'stores and retrieves weather data' do
+      store.write(city: city, data: data, ttl: 5)
+      result = store.read(city: city)
+
+      expect(result[:temperature_degrees]).to eq(22)
+      expect(result[:wind_speed]).to eq(10)
+    end
+
+    it 'expires data after TTL' do
+      store.write(city: city, data: data, ttl: 1)
+      sleep 2
+
+      expect(store.read(city: city)).to be_nil
+    end
+  end
+end
