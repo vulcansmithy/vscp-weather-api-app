56cd0a485d302db9589f26f910dfded120b4a855
Ulysses Legaspi <ulysses.legaspi@gmail.com>
2026-01-29 00:11:30 +0800

    Added resuce for StandardError.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  rescue_from StandardError do
+    render json: { error: 'Internal Server Error' }, status: :internal_server_error
+  end
 end
