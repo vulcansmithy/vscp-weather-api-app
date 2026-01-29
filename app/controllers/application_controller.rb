f9cf75619bee6838bd9bf17cb20872c8e7f79241
Ulysses Legaspi <ulysses.legaspi@gmail.com>
2026-01-29 12:30:52 +0800

    WP: Fix missing top-level documentation comment for class ApplicationController.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# app/application_controller.rb
 class ApplicationController < ActionController::API
   rescue_from StandardError do
     render json: { error: 'Internal Server Error' }, status: :internal_server_error
